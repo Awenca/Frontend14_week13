e7cab082f81f2c81dc980e39624aac26c36add37
Gulnaz <awena.cs@gmail.com>
2022-01-03 21:35:48 +0300

    Added task2 week13

--- assets/styles/index.js
+++ assets/styles/index.js
@@ -2,3 +2,31 @@ function Askname () {
     let name = prompt ('Как тебя зовут?')
     alert(`Привет, ${name}!`);
 }
+
+function sum (){
+    let x = prompt ('Введите первое значение');
+    let y = prompt ('Введите второе значение');
+    result = Number (x) + Number (y);
+    alert (result);
+}
+
+function subtract(){
+    let x = prompt ('Введите первое значение');
+    let y = prompt ('Введите второе значение');
+    result = Number (x) - Number (y);
+    alert (result);
+}
+
+function multiply(){
+    let x = prompt ('Введите первое значение');
+    let y = prompt ('Введите второе значение');
+    result = Number (x) * Number (y);
+    alert (result);
+}
+
+function divide (){
+    let x = prompt ('Введите первое значение');
+    let y = prompt ('Введите второе значение');
+    result = Number (x) / Number (y);
+    alert (result);
+}
